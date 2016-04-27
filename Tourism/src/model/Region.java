a138195403fbc0bf9495cb6b094a7a551ea32f90
WANG Xi <e-wangxi@hotmail.com>
2016-04-27 14:10:58 +0200

    Add model

--- Tourism/src/model/Region.java
+++ Tourism/src/model/Region.java
@@ -6,5 +6,26 @@ import java.util.HashMap;
 public class Region {
 	private ArrayList<Country> countrys;
 	private String name;
-	private HashMap numTourist;
+	private HashMap<Integer,Double> numTourist;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	
+	public void addCountry(Country country){
+		this.countrys.add(country);
+	}
+	public ArrayList<Country> getCountrys(){
+		return this.countrys;
+	}
+	
+	public void addNumTourist(int year,double num){
+		this.numTourist.put(year, num);
+	}
+	public HashMap<Integer,Double> getNumTourist(){
+		return this.numTourist;
+	}
 }
